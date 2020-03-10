3dd8f7d55f0a13537f942a83616216b6d7ff69db
turok01 <44950560+turok01@users.noreply.github.com>
2020-03-10 09:47:49 +0300

    Initial commit

---
+++ src/test/java/com/Igor/spring_taco/SpringTacoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Igor.spring_taco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTacoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
