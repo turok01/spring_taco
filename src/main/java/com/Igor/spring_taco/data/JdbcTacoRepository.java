e99b5a3bf143a9cb94c0a3316075a80c58edbff1
turok01 <44950560+turok01@users.noreply.github.com>
2020-03-24 17:02:44 +0300

    save work

--- src/main/java/com/Igor/spring_taco/data/JdbcTacoRepository.java
+++ src/main/java/com/Igor/spring_taco/data/JdbcTacoRepository.java
@@ -7,12 +7,13 @@ import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.Arrays;
 import java.util.Date;
-
+@Repository
 public class JdbcTacoRepository implements TacoRepository {
     private JdbcTemplate jdbc;
 
